a0186c4afa4c1cb47491bddeed14dd46927b18ad
daesy <daesyramos@hotmail.com>
2020-01-21 11:49:10 -0800

    initial commit from Spring init

---
+++ src/main/java/com/daesy/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.daesy.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
